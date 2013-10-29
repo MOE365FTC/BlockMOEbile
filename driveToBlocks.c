a30883091904f47c603ba7854c4787266be4f54b
DarkVortex <arnav98@gmail.com>
2013-10-29 19:22:17 -0400

    Added IRScore

--- driveToBlocks.c
+++ driveToBlocks.c
@@ -1,9 +1,8 @@
 #pragma config(Hubs,  S1, HTMotor,  HTMotor,  HTMotor,  HTServo)
-#pragma config(Sensor, S1,     ,               sensorI2CMuxController)
 #pragma config(Motor,  mtr_S1_C1_1,     leftDrive,     tmotorTetrix, openLoop, reversed)
-#pragma config(Motor,  mtr_S1_C1_2,     RightDrive,    tmotorTetrix, openLoop)
-#pragma config(Motor,  mtr_S1_C2_1,      ,             tmotorTetrix, openLoop)
-#pragma config(Motor,  mtr_S1_C2_2,      ,             tmotorTetrix, openLoop)
+#pragma config(Motor,  mtr_S1_C1_2,     rightDrive,    tmotorTetrix, openLoop)
+#pragma config(Motor,  mtr_S1_C2_1,     motorF,        tmotorTetrix, openLoop)
+#pragma config(Motor,  mtr_S1_C2_2,     motorG,        tmotorTetrix, openLoop)
 #pragma config(Motor,  mtr_S1_C3_1,     motorF,        tmotorTetrix, openLoop)
 #pragma config(Motor,  mtr_S1_C3_2,     motorG,        tmotorTetrix, openLoop)
 #pragma config(Servo,  srvo_S1_C4_1,    servo1,               tServoNone)
@@ -22,9 +21,7 @@
 
 void initializeRobot()
 {
-  // Place code here to sinitialize servos to starting positions.
-  // Sensors are automatically configured and setup by ROBOTC. They may need a brief time to stabilize.
-
+		GyroInit(gyro, gyroSensor, 0);
   return;
 }
 
