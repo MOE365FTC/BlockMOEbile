8a350202e0780f1e52ddb004dce838cc658082f6
DarkVortex <arnavprasad@arnavs-mbp.home>
2014-02-22 19:09:05 -0500

    replaced IRScoreB with code from IRScoreBackwardsTest
    Updated with newest WORKING code

--- IRScoreB.c
+++ IRScoreB.c
@@ -42,44 +42,54 @@ task main()
 {
 	int timeToWait = requestTimeToWait();
 	initializeRobot();
-
+    
 	waitForStart(); // Wait for the beginning of autonomous phase.
 	GyroInit(g_Gyro, gyro, 0);
 	PidTurnInit(g_PidTurn, leftDrive, rightDrive, MIN_TURN_POWER, g_Gyro, TURN_KP, TURN_TOLERANCE);
 	//Align against right wall, with left edge of left wheels on left edge of third tile (6ft from right wall).
 	countdown(timeToWait);
-
+    
 	//moveForwardInches(60, 1, false, LEFTENCODER); //away from wall
 	//turn(g_PidTurn, 136); //turn to parallel with buckets
 	//clearEncoders(); //clears encoder for the next step
 	//moveBackwardInches(60,4, false, LEFTENCODER);
-
+    
+	clearEncoders();
+	wait1Msec(50);
 	const int totalTics = 7600;//total tics from before IR to end-- DONT CHANGE!
-
-	while(HTIRS2readACDir(IR) != 5 || HTIRS2readACDir(IR) == 0){ //finds the beacon
+    
+	while(HTIRS2readACDir(IR) !<= 6){ //finds the beacon
 		nxtDisplayCenteredTextLine(1,"Direction: %d", HTIRS2readACDir(IR));
-
+        
 		nxtDisplayCenteredTextLine(6,"%d",nMotorEncoder[leftDrive]);
-		if(abs(nMotorEncoder[leftDrive]) >= totalTics-500) break;
-		startBackward(60);
+		//if(abs(nMotorEncoder[leftDrive]) >= totalTics-500) break;
+		startBackward(35);
 	}
-
-	moveBackwardInchesNoReset(60, 4);//reverse back a small amount to correct for IR inaccuracy
-
+	stopDrive();
+	wait1Msec(300);
+	while(HTIRS2readACDir (IR)!= 5){
+		startBackward(15);
+	}
+	moveBackwardInchesNoReset(30, 8);//reverse back a small amount to correct for IR inaccuracy
+    
 	stopDrive();//stops robot
 	servo[dumper] = 30;//dumps the block
 	motor[lift]= 50;//starts the lift up
 	wait1Msec(700);
 	motor[lift]= 0;//stops lift
 	servo[dumper] = servoRestPosition;//resets servo
-	int ticsToMove= totalTics - nMotorEncoder[leftDrive];//tics left after IR
+	int ticsToMove= totalTics - abs(nMotorEncoder[leftDrive]);//tics left after IR
 	nxtDisplayCenteredTextLine(0,"TTM: %d", ticsToMove);
-	while(true){}
+	//while(true){}
 	moveBackwardTics(90, ticsToMove); //move to end after IR
 	turn(g_PidTurn, -85,60); //turn to go towards ramp
 	moveForwardInches(90, 44, false, LEFTENCODER); //forwards to ramp
 	turn(g_PidTurn, 95, 60); //turn to face ramp
 	moveForwardInches(90, 40, false, LEFTENCODER);//onto ramp
+    motor[lift]= -50;//starts the lift down
+	wait1Msec(500);
+	motor[lift]= 0;//stops lift
+    
 	while (true)
 	{}
 }
